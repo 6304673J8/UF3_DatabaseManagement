1ddb2f5d5ee7016b503e3b8ed088fcd741e6578a
6304673J8 <petergeorghe.julca@enti.cat>
2021-02-23 14:31:51 +0100

    Script Permissions

---
+++ permisos_consultas.sql
@@ -0,0 +1,15 @@
+grant select on amongmeme.items to 'consultas'@'localhost';
+grant select on amongmeme.skills to 'consultas'@'localhost';
+grant select on amongmeme.weapons to 'consultas'@'localhost';
+grant select on amongmeme.items_types to 'consultas'@'localhost';
+grant select on amongmeme.stats to 'consultas'@'localhost';
+grant select on amongmeme.weapons_types to 'consultas'@'localhost';
+grant select on amongmeme.armours to 'consultas'@'localhost';
+grant select on amongmeme.armours_types to 'consultas'@'localhost';
+grant select on amongmeme.view_items_types to 'consultas'@'localhost';
+grant select on amongmeme.characters_items to 'consultas'@'localhost';
+grant select on amongmeme.chars_items to 'consultas'@'localhost';
+grant select on amongmeme.characters to 'consultas'@'localhost';
+grant select on amongmeme.characters_weapons to 'consultas'@'localhost';
+grant select on amongmeme.characters_armours to 'consultas'@'localhost';
+
